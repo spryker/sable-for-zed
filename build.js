9751ffaf3f6b661eaa637bacc7635768a327f664
Aleksey Belan <alexey.belan@spryker.com>
2019-01-08 13:12:53 +0200

    TE-535: PR fixes

--- build.js
+++ build.js
@@ -2,6 +2,8 @@
 
 const oryx = require('@spryker/oryx');
 const api = require('./lib');
-const configuration = api.getConfiguration(api.settings);
+const configurationPromise = api.getConfiguration(api.settings);
 
-oryx.build(configuration);
+configurationPromise
+    .then(configuration => oryx.build(configuration))
+    .catch(error => console.error('An error occur while creating configuration', error));
