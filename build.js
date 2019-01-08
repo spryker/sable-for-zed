f8dd1638c7ea7ccd7f0d66eb3bad3f3cc398068d
Aleksey Belan <alexey.belan@spryker.com>
2019-01-08 13:10:56 +0200

    Revert "TE-535: PR fixes"
    This reverts commit c4a58f6

--- build.js
+++ build.js
@@ -2,8 +2,6 @@
 
 const oryx = require('@spryker/oryx');
 const api = require('./lib');
-const configurationPromise = api.getConfiguration(api.settings);
+const configuration = api.getConfiguration(api.settings);
 
-configurationPromise
-    .then(configuration => oryx.build(configuration))
-    .catch(error => console.error('An error occur while creating configuration', error));
+oryx.build(configuration);
