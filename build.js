b3fd440c88ea67c6bfd7191fbee60e5debb5770c
zamlelyi <nikolay.zamlely@spryker.com>
2021-05-20 04:41:21 +0300

    TE-8816: Add deprecated copy assets ability

--- build.js
+++ build.js
@@ -3,6 +3,9 @@
 const oryx = require('@spryker/oryx');
 const api = require('./lib');
 
+// deprecated copyAssetsCallback for backward compatibility only
+const copyAssetsCallback = require('./lib/copy');
+
 api.getConfiguration(api.settings)
-    .then(configuration => oryx.build(configuration))
+    .then(configuration => oryx.build(configuration, copyAssetsCallback))
     .catch(error => console.error('An error occurred while creating configuration', error));
